5ae30f81f7d5ea1a96aa4687a5033dc09091e8f4
cuneguines <47780327+cuneguines@users.noreply.github.com>
2022-10-12 08:53:15 +0100

    committing

--- src/index.php
+++ src/index.php
@@ -11,6 +11,9 @@
 <body>  
 
 <?php
+
+$mysqli = new mysqli('db01', 'root', 'root_pass', 'vishu');
+
 // define variables and set to empty values
 $name = $email = $gender = $comment = $website = "";
 
@@ -30,8 +33,8 @@ function test_input($data) {
 }
 ?>
 
-<h2>PHP Form Validation Example</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+<h2 style=margin-left:40%>PHP Form Validation Example</h2>
+<form style=margin-left:40%;method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
  Name:  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <input type="text" name="name">
   <br><br>
   E-mail: &nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="email">
@@ -45,7 +48,7 @@ function test_input($data) {
   <input type="radio" name="gender" value="male">Male
   <input type="radio" name="gender" value="other">Other
   <br><br>
-  <input type="submit" name="submit" value="Submit">  
+  <input type="submit" name="submit" value="Submit"style='background-color:cornsilk'>  
 </form>
 
 <?php
